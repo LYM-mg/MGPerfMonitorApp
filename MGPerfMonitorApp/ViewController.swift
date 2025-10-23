05f63729ae38834a6baebfc31802da5159e02f33
Nicky <1292043630@qq.com>
2025-10-23 10:42:15 +0800

    init project
    init project

---
+++ MGPerfMonitorApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MGPerfMonitorApp
+//
+//  Created by 刘远明 on 2025/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
